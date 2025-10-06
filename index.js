e89b281749306ce6a9677b8bd9c08cb9657e258d
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-06 15:34:10 +0300

    add to the sum when new card is clicked

--- index.js
+++ index.js
@@ -25,7 +25,13 @@ function startGame() {
   messageEl.textContent = message;
 }
 
-// 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
 function newCard() {
   console.log("Drawing a new card from the deck!");
+
+  // 1. Create a card variable, and hard code its value to a number (2-11)
+  let card = 6;
+  // 2. Add the new card to the sum variable
+  sum += card;
+  // 3. Call startGame()
+  startGame();
 }
