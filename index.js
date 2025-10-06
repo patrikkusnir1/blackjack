2d5e74991a25c109e642d14ba9c336b1709ea0e0
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-06 14:25:17 +0300

    add "new card" button

--- index.js
+++ index.js
@@ -24,3 +24,8 @@ function startGame() {
   }
   messageEl.textContent = message;
 }
+
+// 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
+function newCard() {
+  console.log("Drawing a new card from the deck!");
+}
