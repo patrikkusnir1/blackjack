549bb2cf664bcc920449f3ed3dc92d6a53a02bb1
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-06 12:37:05 +0300

    add boolean value hasBlackjack

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 let firstCard = 10;
-let secondCard = 7;
-let sum = firstCard + secondCard + 4
+let secondCard = 11;
+let sum = firstCard + secondCard;
+let hasBlackjack = false;
 
-// Write the conditional according to these rules:
-
-// if less than or equal to 20 -> "Do you want to draw a new card? 🙂"
-// else if exactly 21 -> "Wohoo! You've got Blackjack! 🥳"
-// else -> "You're out of the game! 😭"
 
 if (sum <= 20) {
   console.log("Do you want to draw a new card? 🙂");
 } else if (sum === 21) {
   console.log("Wohoo! You've got Blackjack! 🥳");
+  hasBlackjack = true;
 } else {
   console.log("You're out of the game! 😭");
 }
+
+// CASH OUT!
+console.log(hasBlackjack)
