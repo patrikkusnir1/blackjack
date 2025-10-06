f8b918495bca13f51f262be019cb5144d600d195
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-06 20:37:34 +0300

    create new array of cards

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let firstCard = 10;
 let secondCard = 4;
-let cards = [firstCard, secondCard]; // array - ordered list of items
+// 1. Create a new array - cards - that contains firstCard and secondCard
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackjack = false;
 let isAlive = true;
@@ -9,36 +10,28 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
-// Create a new function called startGame() that calls renderGame()
-
 function startGame() {
   renderGame();
 }
 
 function renderGame() {
-  // render out firstCard and secondCard
+  // 2. Refer to the cards array when rendering out the cards
   cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
-  // render out all the cards that we have
   sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
-    message = "Do you want to draw a new card?";
+      message = "Do you want to draw a new card?";
   } else if (sum === 21) {
-    message = "You've got Blackjack!";
-    hasBlackjack = true;
+      message = "You've got Blackjack!";
+      hasBlackjack = true;
   } else {
-    message = "You're out of the game!";
-    isAlive = false;
+      message = "You're out of the game!";
+      isAlive = false;
   }
   messageEl.textContent = message;
 }
 
 function newCard() {
-  console.log("Drawing a new card from the deck!");
-
-  // 1. Create a card variable, and hard code its value to a number (2-11)
   let card = 6;
-  // 2. Add the new card to the sum variable
   sum += card;
-  // 3. Call startGame()
   renderGame();
 }
